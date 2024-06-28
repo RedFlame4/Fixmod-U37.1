3a5478dcc8b8d1dde9064ff69dff85b2c5b9e300
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2024-06-28 13:32:29 +0100

    Fix other unintended mag plus weapons

---
+++ dlcmanager.lua
@@ -0,0 +1,4 @@
+
+function GenericDLCManager:has_pd2_clan()
+	return true -- check is broken now so just give it by default
+end
--- weapontweakdata.lua
+++ weapontweakdata.lua
@@ -1,27 +1,27 @@
 Hooks:PostHook(WeaponTweakData, "init", "promod_init", function(self)
-    -- NPC BERETTA
 	self.beretta92_npc.has_suppressor = "suppressed_b"
 
-    -- NPC BENELLI
 	self.benelli_npc.sounds.prefix = "benelli_m4_npc" -- actually use benelli sounds
 
-    -- NPC MAC11
 	self.mac11_npc.sounds.prefix = "mac10_npc" -- actually use mac11 sounds
 
-	-- NPC MOSCONI
 	self.mossberg_npc = clone(self.huntsman_npc) -- huntsman_npc makes far more sense for the weapon
 
-	-- NPC S552
 	self.s552_npc.muzzleflash_silenced = "effects/payday2/particles/weapons/9mm_auto_silence" -- chunky suppressor
 	self.s552_npc.has_suppressor = "suppressed_c" -- chunky suppressor
 
-	-- PLAYER LOCOMOTIVE
-	self.serbu.upgrade_blocks = {
+	local clip_upgrade_block = {
 		weapon = {
-			"clip_ammo_increase" -- this isn't a mag-fed weapon
+			"clip_ammo_increase"
 		}
 	}
 
-	-- PLAYER SAW
+	-- these aren't mag-fed weapons
+	self.r870.upgrade_blocks = clip_upgrade_block
+	self.serbu.upgrade_blocks = clip_upgrade_block
+	self.striker.upgrade_blocks = clip_upgrade_block
+	self.benelli.upgrade_blocks = clip_upgrade_block
+	self.ksg.upgrade_blocks = clip_upgrade_block
+
 	self.saw.sounds.dryfire = nil -- makes no sense for it to play a dryfire sound
 end)
