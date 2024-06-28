0e336732f0668b509fa4a1b892bd53eb3d83b6b9
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2024-06-28 23:19:09 +0100

    Fix formatting

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -1,16 +1,16 @@
 -- Fix shoot action sync not properly ending shoot actions when playing as client
 function UnitNetworkHandler:action_aim_state(unit, state)
-    if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character(unit) then
-        return
-    end
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character(unit) then
+		return
+	end
 
-    if state then
-        unit:movement():action_request({
-            block_type = "action",
-            body_part = 3,
-            type = "shoot"
-        })
-    else
-        unit:movement():sync_action_aim_end()
-    end
+	if state then
+		unit:movement():action_request({
+			block_type = "action",
+			body_part = 3,
+			type = "shoot"
+		})
+	else
+		unit:movement():sync_action_aim_end()
+	end
 end
