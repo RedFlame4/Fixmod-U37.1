739e3311bca89a53a46213445a6ac60df8a8bd54
RedFlame <76979559+RedFlamer@users.noreply.github.com>
2025-05-06 00:58:31 +0100

    Fix

--- explosionmanager.lua
+++ explosionmanager.lua
@@ -118,9 +118,9 @@ function ExplosionManager:detect_and_give_dmg( params )
 				if ray_hit and hit_unit:base() and hit_unit:base()._tweak_table and not hit_unit:character_damage():dead() then
 					type = hit_unit:base()._tweak_table
 
-					if CopDamage:_type_civilian(type) then -- HACK!
+					if CopDamage.is_civilian(type) then
 						count_civilians = count_civilians + 1
-					elseif CopDamage:_type_gangster(type) then -- HACK!
+					elseif CopDamage.is_gangster(type) then
 						count_gangsters = count_gangsters + 1
 					elseif not table.contains(CriminalsManager.character_names(), type) then
 						count_cops = count_cops + 1
@@ -159,9 +159,9 @@ function ExplosionManager:detect_and_give_dmg( params )
 					if hit_unit:base() and hit_unit:base()._tweak_table and hit_unit:character_damage():dead() then
 						type = hit_unit:base()._tweak_table
 
-						if CopDamage:_type_civilian(type) then -- HACK!
+						if CopDamage.is_civilian(type) then
 							count_civilian_kills = count_civilian_kills + 1
-						elseif CopDamage:_type_gangster(type) then -- HACK!
+						elseif CopDamage.is_gangster(type) then
 							count_gangster_kills = count_gangster_kills + 1
 						elseif not table.contains(CriminalsManager.character_names(), type) then
 							count_cop_kills = count_cop_kills + 1
