dd979fe5e82e7668bead4436908c3a7ac1386ce1
RedFlame <temporarytemporary2222222222@gmail.com>
2025-05-11 13:10:19 +0100

    U99.2 changes

---
+++ civilianlogicescort.lua
@@ -0,0 +1,26 @@
+function CivilianLogicEscort._begin_advance_action(data, my_data)
+	--CopLogicAttack._correct_path_start_pos(data, my_data.advance_path) -- not needed anymore, handled in copactionwalk
+
+	local objective = data.objective
+	local haste = objective and objective.haste or "run
+	local new_action_data = {
+		type = "walk",
+		nav_path = my_data.advance_path,
+		variant = haste,
+		body_part = 2,
+		end_rot = end_rot
+	}
+
+	if my_data.coarse_path_index >= #my_data.coarse_path - 1 then
+		new_action_data.end_rot = objective.rot
+	end
+
+	my_data.advancing = data.unit:brain():action_request(new_action_data)
+
+	if my_data.advancing then
+		data.brain:rem_pos_rsrv("path")
+		my_data.advance_path = nil
+	else
+		debug_pause("[CivilianLogicEscort._begin_advance_action] failed to start")
+	end
+end
--- groupaitweakdata.lua
+++ groupaitweakdata.lua
@@ -8,34 +8,40 @@ Hooks:PostHook(GroupAITweakData, "_init_unit_categories", "promod_init_unit_cate
 	}
 
 	-- Unit category fixes
-	self.unit_categories.CS_cop_C45_R870.units = {
+	self.unit_categories.CS_cop_C45_R870.unit_types.america = {
 		Idstring("units/payday2/characters/ene_cop_1/ene_cop_1"),
 		Idstring("units/payday2/characters/ene_cop_2/ene_cop_2"),
 		Idstring("units/payday2/characters/ene_cop_4/ene_cop_4")
 	}
-	self.unit_categories.CS_cop_stealth_MP5.units = {
+	self.unit_categories.CS_cop_stealth_MP5.unit_types.america = {
 		Idstring("units/payday2/characters/ene_cop_3/ene_cop_3") -- cop HRT, not a random bronco blue
 	}
 	self.unit_categories.CS_cop_MP5_R870 = {
-		units = {
-			Idstring("units/payday2/characters/ene_cop_3/ene_cop_3"),
-			Idstring("units/payday2/characters/ene_cop_4/ene_cop_4")
+		unit_types = {
+			america = {
+				Idstring("units/payday2/characters/ene_cop_3/ene_cop_3"),
+				Idstring("units/payday2/characters/ene_cop_4/ene_cop_4")
+			},
+			russia = {
+				Idstring("units/pd2_dlc_mad/characters/ene_akan_cs_cop_akmsu_smg/ene_akan_cs_cop_akmsu_smg"),
+				Idstring("units/pd2_dlc_mad/characters/ene_akan_cs_cop_r870/ene_akan_cs_cop_r870")
+			}
 		},
 		access = access_type_walk_only
 	}
 
 	if difficulty_index >= 6 then
-		self.unit_categories.FBI_swat_M4.units = {
+		self.unit_categories.FBI_swat_M4.unit_types.america = {
 			Idstring("units/payday2/characters/ene_city_swat_1/ene_city_swat_1"), -- rifle
 			Idstring("units/payday2/characters/ene_city_swat_3/ene_city_swat_3") -- ump
 		}
-		self.unit_categories.FBI_swat_R870.units = {
+		self.unit_categories.FBI_swat_R870.unit_types.america = {
 			Idstring("units/payday2/characters/ene_city_swat_2/ene_city_swat_2") -- benelli
 		}
 	end
 
-	self.unit_categories.CS_heavy_M4_w.units = self.unit_categories.CS_heavy_M4.units -- ensure it's always consistent with acrobatic
-	self.unit_categories.FBI_heavy_G36_w.units = self.unit_categories.FBI_heavy_G36.units -- ensure it's always consistent with acrobatic
+	self.unit_categories.CS_heavy_M4_w.unit_types = self.unit_categories.CS_heavy_M4.unit_types -- ensure it's always consistent with acrobatic
+	self.unit_categories.FBI_heavy_G36_w.unit_types = self.unit_categories.FBI_heavy_G36.unit_types -- ensure it's always consistent with acrobatic
 end)
 
 Hooks:PostHook(GroupAITweakData, "_init_enemy_spawn_groups", "promod_init_enemy_spawn_groups", function(self, difficulty_index)
