1824efa30778546b6baf2683ab69aab0b9d6b050
RedFlame <temporarytemporary2222222222@gmail.com>
2025-05-11 13:10:19 +0100

    U99.2 changes

--- missionendstate.lua
+++
@@ -1,5 +0,0 @@
-function MissionEndState:play_finishing_sound(success)
-	if not self._server_left and not success and managers.groupai:state():bain_state() then
-		managers.dialog:queue_dialog("Play_ban_g01x", {}) -- doesn't play in U76, thanks Overkill!
-	end
-end
